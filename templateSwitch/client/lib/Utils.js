bba23c6f7fa93851667c74864cc9ea76aed8171d
maxhodges <max@whiterabbitjapan.com>
2015-04-23 02:07:53 +0900

    Refactor seed function to use array.

--- templateSwitch/client/lib/Utils.js
+++ templateSwitch/client/lib/Utils.js
@@ -3,14 +3,15 @@ Seed = {};
 Seed.go = function() {
   Meteor.call('removeAll');
 
-  var rest = {name: "Chinese", phone: "111-1111", status: "*"};
-  Meteor.call('addRestaurant', rest);
-  rest = {name: "Japanese", phone: "222-1111", status: "**"};
-  Meteor.call('addRestaurant', rest);
-  rest = {name: "Thai", phone: "333-1111", status: "***"};
-  Meteor.call('addRestaurant', rest);
-  rest = {name: "Indonesian", phone: "444-1111", status: "****"};
-  Meteor.call('addRestaurant', rest);
-  rest = {name: "Italian", phone: "555-1111", status: "*****"};
-  Meteor.call('addRestaurant', rest);
+  var seed = [{name: "Chinese", phone: "111-1111", status: "*"},
+    rest = {name: "Japanese", phone: "222-1111", status: "**"},
+    rest = {name: "Thai", phone: "333-1111", status: "***"},
+    rest = {name: "Indonesian", phone: "444-1111", status: "****"},
+    rest = {name: "Italian", phone: "555-1111", status: "*****"}];
+
+  var arrayLength = seed.length;
+  for (var i = 0; i < arrayLength; i++) {
+    Meteor.call('addRestaurant', seed[i]);
+  }
+
 };
